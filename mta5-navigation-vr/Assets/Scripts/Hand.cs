99c716bf35824e6aebe02ce26ddb3441f2e7b7cf
reptinias <32259405+reptinias@users.noreply.github.com>
2021-11-25 14:56:29 +0100

    Hand Update
    Update to Hands

--- mta5-navigation-vr/Assets/Scripts/Hand.cs
+++ mta5-navigation-vr/Assets/Scripts/Hand.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 [RequireComponent(typeof(Animator))]
 public class Hand : MonoBehaviour
@@ -18,13 +19,22 @@ public class Hand : MonoBehaviour
     public float animationSpeed;
 
     //physics variables
-    [SerializeField] private GameObject followObject;
+    [SerializeField] private ActionBasedController controller;
     [SerializeField] private float followSpeed = 30f;
     [SerializeField] private float rotateSpeed = 100f;
     [SerializeField] private Vector3 positionOffset;
     [SerializeField] private Vector3 rotationOffset;
+    [SerializeField] private Transform palm;
+    [SerializeField] private float reachDistance = 0.1f, jointDistance = 0.05f;
+    [SerializeField] private LayerMask grabbableLayer;
+
     private Transform followTarget;
     private Rigidbody body;
+
+    private bool isGrabbing;
+    private GameObject heldObject;
+    private Transform grabPoint;
+    private FixedJoint joint1, joint2;
 
     // Start is called before the first frame update
     void Start()
@@ -34,11 +44,16 @@ public class Hand : MonoBehaviour
         mesh = GetComponentInChildren<SkinnedMeshRenderer>();
 
         //physics
-        followTarget = followObject.transform;
+        followTarget = controller.gameObject.transform;
         body = GetComponent<Rigidbody>();
         body.collisionDetectionMode = CollisionDetectionMode.Continuous;
         body.interpolation = RigidbodyInterpolation.Interpolate;
         body.mass = 20f;
+        body.maxAngularVelocity = 20f;
+
+        //input Setup
+        controller.selectAction.action.started += Grab;
+        controller.selectAction.action.canceled += Release;
 
         //Teleport hands
         body.position = followTarget.position;
@@ -54,7 +69,7 @@ public class Hand : MonoBehaviour
 
     private void PhysicsMove() {
         //position
-        var positionWithOffset = followTarget.position + positionOffset;
+        var positionWithOffset = followTarget.TransformPoint(positionOffset);
         var distance = Vector3.Distance(positionWithOffset, transform.position);
         body.velocity = (positionWithOffset - transform.position).normalized * (followSpeed * distance);
 
@@ -64,6 +79,101 @@ public class Hand : MonoBehaviour
         q.ToAngleAxis(out float angle, out Vector3 axis);
         body.angularVelocity = axis * (angle * Mathf.Deg2Rad * rotateSpeed);
 
+    }
+
+    private void Grab(UnityEngine.InputSystem.InputAction.CallbackContext context) {
+        if (isGrabbing || heldObject) return;
+
+        Collider[] grabbalbeColliders = Physics.OverlapSphere(palm.position, reachDistance, grabbableLayer);
+        if (grabbalbeColliders.Length < 1) return;
+
+        var objectToGrab = grabbalbeColliders[0].transform.gameObject;
+
+        var objectBody = objectToGrab.GetComponent<Rigidbody>();
+
+        if (objectBody != null)
+        {
+            heldObject = objectBody.gameObject;
+        }
+        else {
+            objectBody = objectToGrab.GetComponentInParent<Rigidbody>();
+            if (objectBody != null)
+            {
+                heldObject = objectBody.gameObject;
+            }
+            else return;
+        }
+        StartCoroutine(GrabObject(grabbalbeColliders[0], objectBody));
+    }
+
+    private IEnumerator GrabObject(Collider collider, Rigidbody targetBody) {
+        isGrabbing = true;
+
+        //Create a grab point
+        grabPoint = new GameObject().transform;
+        grabPoint.position = collider.ClosestPoint(palm.position);
+        grabPoint.parent = heldObject.transform;
+
+        //Move hand to grab point
+        followTarget = grabPoint;
+
+        //Wait for hand to reach grab point
+        while (grabPoint != null && Vector3.Distance(grabPoint.position, palm.position) > jointDistance && isGrabbing) {
+            yield return new WaitForEndOfFrame();
+        }
+
+        //Freeze hand and object motion
+        body.velocity = Vector3.zero;
+        body.angularVelocity = Vector3.zero;
+
+        targetBody.velocity = Vector3.zero;
+        targetBody.angularVelocity = Vector3.zero;
+
+        targetBody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        targetBody.interpolation = RigidbodyInterpolation.Interpolate;
+
+        //Attach joints
+        //Cross joint method for at connection er mere stabil
+        //Hand to object
+        joint1 = gameObject.AddComponent<FixedJoint>();
+        joint1.connectedBody = targetBody;
+        joint1.breakForce = float.PositiveInfinity;
+        joint1.breakTorque = float.PositiveInfinity;
+
+        joint1.connectedMassScale = 1;
+        joint1.massScale = 1;
+        joint1.enableCollision = false;
+        joint1.enablePreprocessing = false;
+
+        //Object to hand
+        joint2 = heldObject.AddComponent<FixedJoint>();
+        joint2.connectedBody = body;
+        joint2.breakForce = float.PositiveInfinity;
+        joint2.breakTorque = float.PositiveInfinity;
+             
+        joint2.connectedMassScale = 1;
+        joint2.massScale = 1;
+        joint2.enablePreprocessing = false;
+        joint2.enableCollision = false;
+
+        //Reset follow target
+        followTarget = controller.gameObject.transform;
+    }
+
+    private void Release(UnityEngine.InputSystem.InputAction.CallbackContext context) {
+        if (joint1 != null) Destroy(joint1);
+        if (joint2 != null) Destroy(joint2);
+        if (grabPoint != null) Destroy(grabPoint.gameObject);
+
+        if (heldObject != null) {
+            var targetBody = heldObject.GetComponent<Rigidbody>();
+            targetBody.collisionDetectionMode = CollisionDetectionMode.Discrete;
+            targetBody.interpolation = RigidbodyInterpolation.None;
+            heldObject = null;
+        }
+
+        isGrabbing = false;
+        followTarget = controller.gameObject.transform;
     }
 
     internal void SetGrip(float v)
